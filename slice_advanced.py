1a6bb0a60e657a8b663ffe81c4ae9ffe8d59460d
Lautaro-Agazzi <lagazzi@mail.austral.edu.ar>
2025-08-25 12:00:03 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,9 +2,6 @@ def slice_advanced():
     texto = input("Ingrese un texto: ")
     rst = texto[4::2]
     print(rst)
-    texto = input("Ingrese un numero: ")
-    rst = texto[4::2]
-    print(rst)
 
 slice_advanced()
 
